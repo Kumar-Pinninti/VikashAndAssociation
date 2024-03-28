a397eac3a089cf85808a7802327eac273fba58a4
Kumar_Pinninti <kumar.cam143@gmail.com>
2024-03-28 20:54:35 +0530

    mar 28 code update

---
+++ src/app/modules/contact/components/contact-us/contact-us.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus }  from '@emailjs/browser';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css'],
+})
+export class ContactUsComponent implements OnInit {
+  name: string = '';
+  to_name: string = 'Kumar Pinninti'
+  email: string = '';
+  subject: string = '';
+  message: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  sendEmail(): void {
+    emailjs.send("service_e0srr5k","template_54iix0g", {
+      to_name: this.to_name,
+      from_name: this.name,
+      from_email: this.email,
+      subject: this.subject,
+      message: this.message
+    }, 'sMRt9LsGW9jWhelcw')
+      .then((response: EmailJSResponseStatus) => {
+        console.log('Email sent:', response);
+        alert('Email sent successfully!');
+        this.clearForm();
+      }, (error) => {
+        console.error('Error sending email:', error);
+        alert('Failed to send email. Please try again later.');
+      });
+  }
+
+  clearForm(): void {
+    this.name = '';
+    this.email = '';
+    this.subject = '';
+    this.message = '';
+  }
+
+  // disable the send button until all fields are filled
+  isFormValid(): boolean {
+    return !!this.name && !!this.email && !!this.subject && !!this.message;
+  }
+
+
+  // Regular expression to validate email format
+  isValidEmail(): boolean {
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+    return emailPattern.test(this.email);
+  }
+}
--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactUsComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     ContactRoutingModule
   ]
 })
